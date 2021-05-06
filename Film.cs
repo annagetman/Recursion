e794dd84da1d2e631a93cfbb88df35cfa08b24f1
Ann <annaporik12@gmail.com>
2021-05-06 14:06:26 +0300

    create film

---
+++ Film.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Recursion
+{
+    public class Film
+    {  
+        public string FilmName { get; set; }
+       
+        public int FilmDuration { get; set; }
+    }
+}
+
