2ea07008d1a92d29fb0a56a25e430c8d41364a4e
Ann <annaporik12@gmail.com>
2021-05-08 23:14:07 +0300

    added new classes

--- Program.cs
+++ Program.cs
@@ -5,12 +5,17 @@ namespace Recursion
 {
     class Program
     {
+        
+
+
         static List<Film> Films = new List<Film> { };
         static int _countOfFilms;
         static int _filmDuration;
         static string _filmName;
         static void Main(string[] args)
         {
+            Console.WriteLine(DateTime.Now);
+
             Console.WriteLine("Enter count of films");
             _countOfFilms = Int32.Parse(Console.ReadLine());
 
