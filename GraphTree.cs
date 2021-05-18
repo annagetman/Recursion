59313331924441fa615ccfe6416ce8e8eec2ff3d
Ann <annaporik12@gmail.com>
2021-05-18 13:07:28 +0300

    update class PrintSchedule

--- GraphTree.cs
+++ GraphTree.cs
@@ -20,7 +20,45 @@ namespace CinemaRecursion
         }
         public void CreateGraph(Node node)
         {
+            foreach (var film in movieList)
+            {
+                if (node.RemainingTime >= film.Duration)
+                {
+                    List<Film> tmp = new List<Film>(node.CurrentFilms);
+                    tmp.Add(film);
+                    Node newNode = new Node(node.RemainingTime - film.Duration, tmp);
+                    node.AddNext(newNode);
+                    CreateGraph(newNode);
+                }
+            }
+            bool b = true;
+            foreach (var item in movieList)
+            {
+                if (node.RemainingTime >= item.Duration)
+                { b = false; }
+            }
+            if (b)
+            {
+                TableWithTreeTime currentTable = new TableWithTreeTime();
+                currentTable.FreeTime = node.RemainingTime;
+                currentTable.Table = new List<Film>();
+                foreach (var value in node.CurrentFilms)
+                {
+                    currentTable.Table.Add(new Film { Name = value.Name, Duration = value.Duration });
+                }
+                allTableWithFreeTime.Add(currentTable);
+            }
+        }
 
+        public List<TableWithTreeTime> CreateOptimalTable()
+        {
+            List<TableWithTreeTime> result = new List<TableWithTreeTime>(allTableWithFreeTime);
+
+            for (int i = allTableWithFreeTime.Count - 1; i > 0; i--)
+            {
+                result.RemoveAt(i);
+            }
+            return result;
         }
     }
 }
--- PrintSchedule.cs
+++ PrintSchedule.cs
@@ -6,5 +6,22 @@ namespace CinemaRecursion
 {
     public class PrintSchedule
     {
-    }
+        public static void PrintTable(List<TableWithTreeTime> allTablesWithFreeTime)
+        {
+            Console.WriteLine("\nОптимальное расписание работы зала кинотеатра:");
+            foreach (var currentTable in allTablesWithFreeTime)
+            {
+                DateTime startFilmTime = new DateTime(2021, 05, 12, 10, 00, 00);
+                Console.WriteLine($"\nРасписание работы зала :");
+                foreach (var currentFilm in currentTable.Table)
+                {
+                    DateTime endFilmTime = startFilmTime.AddMinutes(currentFilm.Duration);
+                    Console.WriteLine($"{startFilmTime.ToShortDateString()}-{endFilmTime.ToShortDateString()} {currentFilm.Name}, продолжительность:{currentFilm.Duration} минут");
+                    startFilmTime = endFilmTime;
+                }
+                Console.WriteLine($"Оставшееся свободное время в зале: {currentTable.FreeTime} минут");
+                Console.WriteLine("\n*************");
+            }
+        }
+    }    
 }
