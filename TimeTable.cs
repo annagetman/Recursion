da5cae0ca7967b19bb06bdbd4459950d35a76b2d
Ann <annaporik12@gmail.com>
2021-05-20 19:33:00 +0300

    added class TimeTable

---
+++ TimeTable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CinemaRecursion
+{
+    public class TimeTable
+    {
+        public int FreeTime { get; set; }
+
+        public List<Film> Table { get; set; }
+    }
+}
