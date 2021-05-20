c839f340bb14a79cfb2ee17620e1cf8145efe618
Ann <annaporik12@gmail.com>
2021-05-20 19:26:21 +0300

    Added Equals and ToString to Film

--- Film.cs
+++ Film.cs
@@ -9,6 +9,33 @@ namespace CinemaRecursion
         public string Name { get; set; }
        
         public int Duration { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            bool result = false;
+            if (obj is Film)
+            {
+                Film film = (Film)obj;
+
+                if (Duration == film.Duration && Name ==film.Name)
+                {
+                    result = true;
+                }
+            }
+            return result;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            int hour = Duration / 60;
+            int minute = Duration % 60;
+            string strMinute = minute == 0 ? "00" : minute.ToString();
+            string time = $"{hour}:{strMinute}";
+            result.Append($"{time} {Name} ");
+
+            return result.ToString().Trim();
+        }
     }
 }
 
